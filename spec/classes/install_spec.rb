1469266438558cd667c602a0f0897b8da91517e8
tragiccode <michael@tragiccode.com>
2017-06-10 22:34:20 -0500

    Update my tests

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -8,17 +8,17 @@ describe 'vscode::install' do
     it { should contain_class('vscode') }
     it { should contain_class('vscode::install') }
 
-    it { should contain_file('C:\Temp\VSCodeSetup-1.12.2.exe').with({
+    it { should contain_file('C:\Windows\Temp\VSCodeSetup-1.12.2.exe').with({
         :ensure => 'file',
         :source => 'https://az764295.vo.msecnd.net/stable/19222cdc84ce72202478ba1cec5cb557b71163de/VSCodeSetup-1.12.2.exe',
     })}
 
     it { should contain_package('Microsoft Visual Studio Code').with({
         :ensure            => 'present',
-        :source            => 'C:\Temp\VSCodeSetup-1.12.2.exe',
+        :source            => 'C:\Windows\Temp\VSCodeSetup-1.12.2.exe',
         :install_options   => ['/verysilent', '/mergetasks=!runCode,desktopicon,quicklaunchicon,addcontextmenufiles,addcontextmenufolders,addtopath', { '/log' => 'C:\\VSCodeSetup-install.log', }],
         :uninstall_options => ['/verysilent'],
-    }).that_requires('File[C:\Temp\VSCodeSetup-1.12.2.exe]')}
+    }).that_requires('File[C:\Windows\Temp\VSCodeSetup-1.12.2.exe]')}
 
   end
 
@@ -33,15 +33,15 @@ describe 'vscode::install' do
     it { should contain_class('vscode') }
     it { should contain_class('vscode::install') }
 
-    it { should contain_file('C:\Temp\VSCodeSetup-1.12.2.exe').with({
+    it { should contain_file('C:\Windows\Temp\VSCodeSetup-1.12.2.exe').with({
         :ensure => 'absent',
         :source => 'https://az764295.vo.msecnd.net/stable/19222cdc84ce72202478ba1cec5cb557b71163de/VSCodeSetup-1.12.2.exe',
     })}
 
     it { should contain_package('Microsoft Visual Studio Code').with({
         :ensure => 'absent',
-        :source => 'C:\Temp\VSCodeSetup-1.12.2.exe',
-    }).that_requires('File[C:\Temp\VSCodeSetup-1.12.2.exe]')}
+        :source => 'C:\Windows\Temp\VSCodeSetup-1.12.2.exe',
+    }).that_requires('File[C:\Windows\Temp\VSCodeSetup-1.12.2.exe]')}
 
   end
 end
